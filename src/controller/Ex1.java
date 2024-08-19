0d9c167052da355a816ad82a40a10986e1da40c0
breno <breno.sales@fatec.sp.gov.br>
2024-08-19 00:57:32 -0300

    Exercicio 1 - atualização

--- src/controller/Ex1.java
+++ src/controller/Ex1.java
@@ -6,9 +6,11 @@ public class Ex1 {
     }
     public static int contaDigitos(int num){
         if (num < 10 && num > - 10){
+            // Retorna 1 para números entre -9 e 9.
             return 1;
         }
 
+        // Conta 1 e remove o ultimo digito para contar os restantes
         return 1 + contaDigitos(num / 10);
     }
 }
